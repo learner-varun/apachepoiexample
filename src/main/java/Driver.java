69ee20e153cd992328ad7501db27ccee552e84fa
Varun PK <learningzoneofvarun@gmail.com>
2019-11-23 22:39:21 +0530

    Code update for driver

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Driver {
+   private static WebDriver seleniumWebDriver;
+    public static void initializeDriver()
+    {
+        WebDriverManager.chromedriver().version("2.40").setup();
+seleniumWebDriver = new ChromeDriver();
+    }
+    public static WebDriver getDriver()
+    {
+        return seleniumWebDriver;
+    }
 }
